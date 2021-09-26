a9d57e6eb033e951fdd073f6a20e4796d94131b3
marcellasmoreira@gmail.com <marcellasmoreira@gmail.com>
2021-09-26 18:26:49 +0100

    new

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -72,24 +72,18 @@ export class LoginComponent implements OnInit {
       password: ['', Validators.required],
     });
 
-    // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  // convenience getter for easy access to form fields
   get f() {
     return this.loginForm.controls;
   }
 
   onSubmit() {
     this.submitted = true;
-
-    // stop here if form is invalid
     if (this.loginForm.invalid) {
       return;
     }
-
-    this.loading = true;
     this.authenticationService
       .login(this.f.username.value, this.f.password.value)
       .pipe(first())
